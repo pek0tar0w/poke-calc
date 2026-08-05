3c679bd87bae2d6d7c11d9f38703fef53440d02c
Taro Kimura <devkim999@gmail.com>
2026-08-05 13:20:41 +0900

    refactor: 共通表示名の配置を整理

---
+++ packages/data/src/common/names/index.ts
@@ -0,0 +1,5 @@
+export { natureNames } from "./nature-names.js";
+export { screenNames } from "./screen-names.js";
+export { terrainNames } from "./terrain-names.js";
+export { typeNames } from "./type-names.js";
+export { weatherNames } from "./weather-names.js";
